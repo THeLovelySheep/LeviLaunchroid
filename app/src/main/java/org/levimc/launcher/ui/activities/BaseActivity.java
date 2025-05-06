d32c00cd3c1b97efae6d8a5548a6898464d70338
Esczvefs <guxiang@litebds.com>
2025-05-06 12:25:30 +0800

    fix: fix theme toggle functionality

--- app/src/main/java/org/levimc/launcher/ui/activities/BaseActivity.java
+++ app/src/main/java/org/levimc/launcher/ui/activities/BaseActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.levimc.launcher.util.ThemeManager;
+
 public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        ThemeManager themeManager = new ThemeManager(this);
+        themeManager.setThemeMode(themeManager.getCurrentMode());
         super.onCreate(savedInstanceState);
     }
 
